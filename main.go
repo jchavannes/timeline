d1004789ca471f6741382c687c7f77a681a4f0a0
Jason Chavannes <jason.chavannes@gmail.com>
2017-04-11 23:32:00 -0700

    Use JGo web and remove older stuff.

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"git.jasonc.me/pub/jgo/site"
+	"github.com/jchavannes/jgo/web"
 	"gopkg.in/yaml.v2"
 	"log"
-	"net/http"
-	"strconv"
 	"io/ioutil"
-	"time"
-	"math"
 	"github.com/jchavannes/browser-history-parser/wikipedia"
 )
 
@@ -18,10 +13,6 @@ type event struct {
 	Actual int64
 	Label  string
 	Source string
-}
-
-func (e event) GetDate() string {
-	return converterUniverseTimeToCosmicCalendar(e.Actual, true)
 }
 
 func (e event) GetSourceText() string {
@@ -36,26 +27,10 @@ type window struct {
 	Max int64
 }
 
-func (w window) GetStart() string {
-	return converterUniverseTimeToCosmicCalendar(w.Min, false)
-}
-
-func (w window) GetEnd() string {
-	return converterUniverseTimeToCosmicCalendar(w.Max, false)
-}
-
 type period struct {
 	Name string
 	Min  int64
 	Max  int64
-}
-
-func (p period) GetStart() string {
-	return converterUniverseTimeToCosmicCalendar(p.Min, false)
-}
-
-func (p period) GetEnd() string {
-	return converterUniverseTimeToCosmicCalendar(p.Max, false)
 }
 
 type era struct {
@@ -70,72 +45,35 @@ type events struct {
 	Eras []era
 }
 
-func converterUniverseTimeToCosmicCalendar(yearsAgo int64, useOffset bool) string {
-	if useOffset && yearsAgo < 10000 {
-		yearOffset := int64(time.Now().Year() - 2000)
-		yearsAgo += yearOffset
+var (
+	indexRoute = web.Route{
+		Pattern: "/",
+		Handler: func(r *web.Response) {
+			r.Helper["Events"] = getEvents()
+			r.Render()
+		},
 	}
-	unixMin := float64(1420070400) // 2015-01-01 00:00:00
-	unixMax := float64(1451606400) // 2016-01-01 00:00:00
-	universeMax := float64(13820000000) // 13.82 billion years
 
-	seconds := ((universeMax - float64(yearsAgo)) / universeMax) * (unixMax - unixMin)
-	unixTs := unixMin + seconds + 28800 // Offset for PST
-
-	sec, dec := math.Modf(unixTs)
-	ts := time.Unix(int64(sec), int64(dec * 10e8)).Format("2006-01-02T15:04:05.999999Z07:00")
-	return ts
-}
-
-const port = 2040
-
-func main() {
-	fmt.Printf("Starting timeline web server on port %d\n", port)
-
-	// Requests
-	server := http.NewServeMux()
-	server.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	getEvents = func () events {
 		data, err := ioutil.ReadFile("events.yml")
 		check(err)
-
 		e := events{}
 		err = yaml.Unmarshal(data, &e)
 		check(err)
+		return e
+	}
+)
 
-		renderer, err := site.GetRenderer("templates")
-		check(err)
-
-		counter := 0
-		renderer.SetFuncMap(map[string]interface{}{
-			"counter": func() int {
-				counter++
-				return counter
-			},
-		})
-
-		filename := site.GetFilenameFromRequest(r)
-		if len(filename) == 0 {
-			filename = "index"
-		}
-
-		err = renderer.Render([]string{
-			filename + ".html",
-			"404.html",
-		}, w, &e)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Handled request: %#v\n", r.URL)
-	})
-
-	// Static assets
-	fs := http.FileServer(http.Dir("public"))
-	server.Handle("/public/", http.StripPrefix("/public/", fs))
-
-	err := http.ListenAndServe(":" + strconv.Itoa(port), server)
-	check(err)
+func main() {
+	server := web.Server{
+		Port: 2040,
+		TemplatesDir: "web",
+		StaticFilesDir: "web",
+		Routes: []web.Route{
+			indexRoute,
+		},
+	}
+	server.Run()
 }
 
 func check(e error) {
---
+++ web/timeline.js
@@ -0,0 +1,88 @@
+$(function() {
+    var $sources = $(".timelines-list li span");
+    if (localStorage.sourcesDisplay && localStorage.sourcesDisplay === "hide") {
+        hideSources();
+    } else if (localStorage.sourcesDisplay && localStorage.sourcesDisplay === "show") {
+        showSources();
+    } else {
+        inlineSources();
+    }
+
+    function showSources() {
+        $sources.css("display", "block");
+    }
+
+    function hideSources() {
+        $sources.css("display", "none");
+    }
+
+    function inlineSources() {
+        $sources.css("display", "inline");
+    }
+
+    $("#show-sources").click(function() {
+        localStorage.sourcesDisplay = "show";
+        showSources();
+    });
+
+    $("#inline-sources").click(function() {
+        localStorage.sourcesDisplay = "inline";
+        inlineSources();
+    });
+
+    $("#hide-sources").click(function() {
+        localStorage.sourcesDisplay = "hide";
+        hideSources();
+    });
+});
+
+function timeline(eraName, eraLabel, eraData, $timeline) {
+    var appendText = "<h4>" + eraName + " (" + eraLabel + ")</h4><ul>";
+    for (var i = 0; i < eraData.length; i++) {
+        var event = eraData[i];
+        if (event.group === 1) {
+            appendText += "<hr/>";
+            continue;
+        }
+        var sourceHtml = "";
+        if (event.source.length > 0) {
+            if (event.source.substr(0,4) === "http") {
+                sourceHtml = " <a href='" + event.source + "'>[" + event.sourceText + "]</a>";
+            } else {
+                sourceHtml = " <i>[" + event.sourceText + "]</i>";
+            }
+            sourceHtml = "<span>" + sourceHtml + "</span>";
+        }
+        appendText += "<li>" + event.content + sourceHtml + "</li>";
+    }
+    appendText += "</ul>";
+    $timeline.append(appendText);
+}
+
+/**
+ * @typedef {{
+ *   Name: string
+ *   Label: string
+ *   Eras: [Era]
+ *   Events: [Event]
+ *   Start: float
+ *   End: float
+ * }} Timeline
+ */
+
+/**
+ * @typedef {{
+ *  Name: string
+ *  Color: string
+ *  Start: float
+ *  End: float
+ * }} Era
+ */
+
+/**
+ * @typedef {{
+ *   Name: string
+ *   YearsAgo: float
+ *   Label: string
+ * }} Event
+ */
