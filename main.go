583886a056468f7dad638f3144ba1c2feeba8ce8
Jason Chavannes <jason.chavannes@gmail.com>
2017-04-11 23:55:29 -0700

    Title and organization

--- main.go
+++ main.go
@@ -8,30 +8,6 @@ import (
 	"github.com/jchavannes/browser-history-parser/wikipedia"
 )
 
-type event struct {
-	Name   string
-	Actual int64
-	Label  string
-	Source string
-}
-
-func (e event) GetSourceText() string {
-	if ! wikipedia.IsWikipediaUrl(e.Source) {
-		return e.Source
-	}
-	return "wiki/" + wikipedia.ArticleNameFromUrl(e.Source)
-}
-
-type era struct {
-	Name    string
-	Label   string
-	Events  []event
-}
-
-type events struct {
-	Eras []era
-}
-
 var (
 	indexRoute = web.Route{
 		Pattern: "/",
@@ -61,6 +37,30 @@ func main() {
 		},
 	}
 	server.Run()
+}
+
+type events struct {
+	Eras []era
+}
+
+type era struct {
+	Name    string
+	Label   string
+	Events  []event
+}
+
+type event struct {
+	Name   string
+	Actual int64
+	Label  string
+	Source string
+}
+
+func (e event) GetSourceText() string {
+	if ! wikipedia.IsWikipediaUrl(e.Source) {
+		return e.Source
+	}
+	return "wiki/" + wikipedia.ArticleNameFromUrl(e.Source)
 }
 
 func check(e error) {
